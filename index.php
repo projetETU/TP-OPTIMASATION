9870ba3275db9885ddb7f3549428e92f26e682e0
Mendrika Rs <men.malala@gmail.com>
2024-03-10 22:35:19 +0300

    PERFROMANCE

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ else {
 		<title>Welcome to ebuybd online shop</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
 			</head>
 			<!-- Google tag (gtag.js) -->
 <script>
